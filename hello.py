7207fb1a757bcc551c11e8149d34fbe35ae86707
Amod Wani <awani2@ext.uber.com>
2020-12-21 01:25:42 +0530

    example1 hello world

---
+++ hello.py
@@ -0,0 +1,54 @@
+import numpy
+import pyopencl as cl
+import time
+
+TASKS = 64
+
+if __name__ == '__main__':
+
+    print('load program from cl source file')
+    # f = open('hello.cl', 'r', encoding='utf-8')
+    # kernels = ''.join(f.readlines())
+    # f.close()
+
+    kernels = '''
+    __kernel void hello_world(__global int* values) {
+
+        int global_id = get_global_id(0);
+        printf("Hello Taiwan!!! from kernel #%d, got value: %d\n", global_id, values[global_id]);
+
+    }
+    '''
+
+    print('prepare data ... ')
+    start_time = time.time()
+    matrix = numpy.random.randint(low=1, high=101, dtype=numpy.int32, size=TASKS)
+    time_hostdata_loaded = time.time()
+
+    print('create context')
+    ctx = cl.create_some_context()
+    print('create command queue')
+    queue = cl.CommandQueue(ctx, properties=cl.command_queue_properties.PROFILING_ENABLE)
+    time_ctx_queue_creation = time.time()
+
+    # prepare device memory for OpenCL
+    print('prepare device memory for input / output')
+    dev_matrix = cl.Buffer(ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=matrix)
+    time_devicedata_loaded = time.time()
+
+    print('compile kernel code')
+    prg = cl.Program(ctx, kernels).build()
+    time_kernel_compilation = time.time()
+
+    print('execute kernel programs')
+    evt = prg.hello_world(queue, (TASKS, ), (1, ), dev_matrix)
+    print('wait for kernel executions')
+    evt.wait()
+    elapsed = 1e-9 * (evt.profile.end - evt.profile.start)
+    print('done')
+
+    print('Prepare host data took       : {}'.format(time_hostdata_loaded - start_time))
+    print('Create CTX/QUEUE took        : {}'.format(time_ctx_queue_creation - time_hostdata_loaded))
+    print('Upload data to device took   : {}'.format(time_devicedata_loaded - time_ctx_queue_creation))
+    print('Compile kernel took          : {}'.format(time_kernel_compilation - time_devicedata_loaded))
+    print('OpenCL elapsed time          : {}'.format(elapsed))
